7390d269930752c8c18536f7aee1ee62c9aa2345
guodong <>
2018-07-02 18:51:27 +0800

    add logo and text

--- LoginViewController.js
+++ LoginViewController.js
@@ -1,38 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-    Platform,
+    View,
+    Image,
     StyleSheet,
     Text,
-    View
 } from 'react-native';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class LoginViewController extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
-                    {instructions}
-                </Text>
+                <View style={{flexDirection:'row',justifyContent:'space-between',}}>
+
+                    <Image style={styles.exitImg}
+                                              source={require('./images/new_navClose.png')}/>
+
+                    <Text style={styles.register}>
+                        注册
+                    </Text>
+
+                </View>
+
+                <Image  style={styles.imageLogo}
+                        source={require('./images/logo.png')}/>
             </View>
         );
     }
@@ -41,18 +33,31 @@ export default class LoginViewController extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        // alignItems: 'flex-start',
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    exitImg:{
+        width:20,
+        height:20,
+        marginTop:30,
+        marginLeft:10,
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    register: {
+        fontSize: 15,
+        textAlign: 'right',
+        marginTop: 32,
+        marginRight:15,
+        color: 'gray',
+        // alignSelf:'flex-start',
+        // justifyContent:'flex-end',
+        // alignItems:'flex-end',
+        // backgroundColor:'green'
+    },
+    imageLogo: {
+         alignSelf: 'center',   //center
+         width: 100,
+         height: 100,
+         marginTop:20,
     },
 });
